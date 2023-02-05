160890afe992f91ce84ec7653698d41e42e6c8f7
SamidQuliyev <samidquliyev2003@gmail.com>
2023-02-05 23:13:18 +0400

    Init

---
+++ WebApplication3/WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> Products = new List<Product>();
+            Products.Add(new Product { Id = 1, Name = "Phone", Price = 290 });
+            return View();
+        }
+    }
+}
